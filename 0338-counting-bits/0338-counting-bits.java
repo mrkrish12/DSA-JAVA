2e7a7ed54f88d9813ee00adb80f0956e0b267fea
Krishna Agarwal <110688643+mrkrish12@users.noreply.github.com>
2023-09-01 17:59:14 +0530

    Time: 11 ms (13.02%), Space: 46.3 MB (79.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,24 +1,26 @@
 class Solution {
     public int[] countBits(int n) {
-        int res[] = new int[n+1];
+        int arr[] = new int[n+1];
         int j=0;
         for(int i=0;i<=n;i++)
         {
-            res[j++]=add(i);
+            arr[j++] = add(i);
         }
-        return res;
+        return arr;
     }
     public int add(int n)
     {
-        if(n == 0 || n==1)
+        if(n==0 || n==1)
         {
             return n;
         }
-        else if(n%2 == 0)
+        else if(n%2==0)
         {
             return add(n/2);
         }
         else
+        {
             return 1+(add(n/2));
+        }
     }
 }
